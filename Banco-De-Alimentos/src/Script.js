ebbee67d10d24183d3eac9cc6609729ac52175c4
Junicavi <>
2017-11-22 12:01:48 -0500

    Banco de Alimentos Project Update

--- Banco-De-Alimentos/src/Script.js
+++ Banco-De-Alimentos/src/Script.js
@@ -3,7 +3,7 @@ var Modal = document.getElementById('Id1');
 
 // Clickear fuera del modal lo cierra 
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == Modal) {
         Modal.style.display = "none";
     }
 }
