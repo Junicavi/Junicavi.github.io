4848452dc29fc0de2eb1c600294dddb25cff24e6
Junicavi <>
2017-12-18 14:27:42 -0500

    Massive repo update

--- Banco-De-Alimentos/src/Script.js
+++
@@ -1,9 +0,0 @@
-// Obtener el modal
-var Modal = document.getElementById('Id1');
-
-// Clickear fuera del modal lo cierra 
-window.onclick = function(event) {
-    if (event.target == Modal) {
-        Modal.style.display = "none";
-    }
-}
